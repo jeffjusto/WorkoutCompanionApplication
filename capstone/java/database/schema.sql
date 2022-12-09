4e2901e52209ede767fd51d552b1d4ad5443b621
Madalyn Dorismond <mdorismond25@gmail.com>
2022-12-09 11:43:33 -0600

    database

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -23,3 +23,90 @@ primary key (profile_id);
 alter table profile
 add constraint fk_user_id
 foreign key (user_id) references users(user_id);
+
+
+CREATE TABLE IF NOT EXISTS public."Equipment"
+(
+    equipment_id integer NOT NULL,
+    equipmentdescriptors "char",
+    CONSTRAINT "Equipment_pkey" PRIMARY KEY (equipment_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Equipment"
+    OWNER to postgres;
+
+
+CREATE TABLE IF NOT EXISTS public."Log"
+(
+    entry_id integer NOT NULL,
+    user_id integer,
+    "isCheckedIn" boolean,
+    metrics integer,
+    CONSTRAINT "Log_pkey" PRIMARY KEY (entry_id),
+    CONSTRAINT user_id FOREIGN KEY (user_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Log"
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public."Schedule"
+(
+    event_id integer NOT NULL,
+    "eventDate" date,
+    "eventDescription" character(1) COLLATE pg_catalog."default",
+    CONSTRAINT "Schedule_pkey" PRIMARY KEY (event_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Schedule"
+    OWNER to postgres;
+
+    CREATE TABLE IF NOT EXISTS public.equipmentusage
+    (
+        "useEntryID" integer NOT NULL,
+        user_id integer,
+        equipment_id integer,
+        weight integer,
+        reps integer,
+        "dateUsed" date,
+        CONSTRAINT equipmentusage_pkey PRIMARY KEY ("useEntryID"),
+        CONSTRAINT equipment_id FOREIGN KEY ("useEntryID")
+            REFERENCES public."Equipment" (equipment_id) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID,
+        CONSTRAINT user_id FOREIGN KEY (user_id)
+            REFERENCES public.users (user_id) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+    )
+
+    TABLESPACE pg_default;
+
+    ALTER TABLE public.equipmentusage
+        OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.goals
+(
+    goal_id integer NOT NULL,
+    user_id integer,
+    "goalDescriptors" "char",
+    CONSTRAINT goals_pkey PRIMARY KEY (goal_id),
+    CONSTRAINT user_id FOREIGN KEY (user_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.goals
+    OWNER to postgres;
